db38a7abae38205722702f6d123cda44e4060cf6
Divyaank <divyaankt21@gmail.com>
2020-07-12 21:59:05 +0530

    Finished implementing ControlLogic

--- src/sudoku/userinterface/logic/ControlLogic.java
+++ src/sudoku/userinterface/logic/ControlLogic.java
@@ -1,4 +1,68 @@
 package sudoku.userinterface.logic;
 
-public class ControlLogic {
+import sudoku.constants.GameState;
+import sudoku.constants.Messages;
+import sudoku.problemdomain.IStorage;
+import sudoku.problemdomain.SudokuGame;
+import sudoku.userinterface.IUserInterfaceContract;
+
+import java.io.IOException;
+
+public class ControlLogic implements IUserInterfaceContract.EventListener {
+
+    private IStorage storage;
+    private IUserInterfaceContract.View view;
+
+    public ControlLogic(IStorage storage, IUserInterfaceContract.View view) {
+        this.storage = storage;
+        this.view = view;
+    }
+
+    /**
+     * Use Case:
+     * 1. Retrieve current "state" of the data from IStorage
+     * 2. Update it according to the input
+     * 3. Write the result to IStorage
+     * @param x X coordinate of the selected input
+     * @param y Y ...
+     * @param input Which key was entered, One of:
+     *  - Numbers 0-9
+     *
+     */
+    @Override
+    public void onSudokuInput(int x, int y, int input) {
+        try{
+            SudokuGame gameData = storage.getGameData();
+            int[][] newGridState = gameData.getCopyOfGridState();
+            newGridState[x][y] = input;
+
+            gameData = new SudokuGame(
+                    GameLogic.checkForCompletion(newGridState),
+                    newGridState
+            );
+
+            storage.updateGameData(gameData);
+
+            //either way, update the view
+            view.updateSquare(x, y, input);
+
+            //if game is complete, show dialog
+            if (gameData.getGameState() == GameState.COMPLETE)
+                view.showDialog(Messages.GAME_COMPLETE);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            view.showError(Messages.GAME_ERROR);
+        }
+    }
+
+    @Override
+    public void onDialogClick() {
+        try {
+            storage.updateGameData(GameLogic.getNewGame());
+            view.updateBoard(storage.getGameData());
+        } catch (IOException e) {
+            view.showError(Messages.GAME_ERROR);
+        }
+    }
 }
